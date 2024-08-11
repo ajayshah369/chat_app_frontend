5c61a01b62481636ce434671db33e59466be52af
Ajay Kumar Shah <ajaykymarshah998@gmail.com>
2024-08-11 11:28:31 +0530

    Chat bottom complete

--- src/containers/chat.tsx
+++ src/containers/chat.tsx
@@ -1,9 +1,14 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Typography, InputBase } from "@mui/material";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 import { ChatType } from "../store/chatsSlice";
 import { RootState } from "../store";
 import ProfileIcon from "../assets/icons/profile.svg?react";
+import SmileyIcon from "../assets/icons/smiley.svg?react";
+import AttachIcon from "../assets/icons/attach.svg?react";
+import MicIcon from "../assets/icons/mic.svg?react";
+import SendIcon from "../assets/icons/send.svg?react";
 
 type ChatHeaderPropsType = {
   chat: ChatType;
@@ -71,15 +76,110 @@ const ChatHeader = ({ chat }: ChatHeaderPropsType) => {
 };
 
 const ChatBottom = () => {
+  const [text, setText] = useState<string>("");
+
   return (
     <Box
       component='footer'
       sx={{
         padding: "5px 16px",
         bgcolor: "secondary.main",
-        height: "66px",
+        minHeight: "66px",
       }}
-    ></Box>
+      className='flex items-center'
+    >
+      <Box
+        component='div'
+        className='flex items-center justify-center cursor-pointer'
+        sx={{
+          height: "42px",
+          width: "42px",
+          color: "icon.dark",
+        }}
+      >
+        <SmileyIcon
+          style={{
+            height: "26px",
+            width: "26px",
+          }}
+        />
+      </Box>
+
+      <Box
+        component='div'
+        className='flex items-center justify-center cursor-pointer'
+        sx={{
+          height: "46px",
+          width: "46px",
+          color: "icon.dark",
+        }}
+      >
+        <AttachIcon />
+      </Box>
+
+      <Box
+        component='div'
+        className='flex-grow'
+        sx={{
+          margin: "5px 8px",
+          padding: "9px 12px",
+          bgcolor: "secondary.light",
+          borderRadius: "8px",
+        }}
+      >
+        <InputBase
+          name='Message'
+          fullWidth
+          placeholder={"Type a message"}
+          size='small'
+          sx={{
+            fontWeight: 300,
+            fontSize: 15,
+            padding: 0,
+            margin: 0,
+            "::placeholder": {
+              color: "icon.dark",
+              fontSize: 14,
+            },
+            "& input": {
+              padding: 0,
+              margin: 0,
+              color: "secondary.A100",
+            },
+          }}
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+      </Box>
+
+      {text ? (
+        <Box
+          component='div'
+          className='flex items-center justify-center cursor-pointer'
+          sx={{
+            height: "40px",
+            width: "40px",
+            color: "icon.dark",
+          }}
+        >
+          <SendIcon />
+        </Box>
+      ) : (
+        <Box
+          component='div'
+          className='flex items-center justify-center cursor-pointer'
+          sx={{
+            height: "40px",
+            width: "40px",
+            color: "icon.dark",
+          }}
+        >
+          <MicIcon />
+        </Box>
+      )}
+    </Box>
   );
 };
 
@@ -91,15 +191,10 @@ const Chat = () => {
   }
 
   return (
-    <Box component='div' className='h-full'>
+    <Box component='div' className='h-full flex flex-col'>
       <ChatHeader chat={chat} />
 
-      <Box
-        component='main'
-        style={{
-          height: "calc(100% - 60px - 66px)",
-        }}
-      ></Box>
+      <Box component='main' className='flex-grow'></Box>
 
       <ChatBottom />
     </Box>
